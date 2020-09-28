c3572a89d02d27c34c73fc8fa4f7edb1fb466136
YSL-1997 <61572100+YSL-1997@users.noreply.github.com>
2020-09-28 16:22:10 -0500

    Update munch1.c

--- p2/munch1.c
+++ p2/munch1.c
@@ -1,6 +1,8 @@
 /*
-	Yusen Liu, liu797@wisc.edu
-	Yuting Yan, yan86@wisc.edu
+	Authors:
+	Yusen Liu - NetID: liu797, CS login: yusen
+	Yuting Yan - NetID: yan86 , CS login: yuting
+	Project #2 - Shared Memory Producer/Consumer Program
 */
 
 /*
