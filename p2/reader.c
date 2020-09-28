6009bf0a8e0d01b20d46c8043a5a1ec2c18e97e2
YSL-1997 <61572100+YSL-1997@users.noreply.github.com>
2020-09-28 16:17:10 -0500

    Update reader.c

--- p2/reader.c
+++ p2/reader.c
@@ -4,6 +4,6 @@
 */
 
 /*
-The Reader thread will read from standard input, one line at a time. 
-Reader will take the each line of the input and pass it to thread Munch1 through a queue of character strings.
+	The Reader thread will read from standard input, one line at a time. 
+	Reader will take the each line of the input and pass it to thread Munch1 through a queue of character strings.
 */
