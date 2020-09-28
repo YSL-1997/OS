77cf928ba38b19b57391e2c7ba4f84c740fa4ee9
YSL-1997 <61572100+YSL-1997@users.noreply.github.com>
2020-09-28 16:16:46 -0500

    Update munch1.c

--- p2/munch1.c
+++ p2/munch1.c
@@ -4,6 +4,6 @@
 */
 
 /*
-Munch1 will scan the line and replace each space character (not tabs or newlines) with an asterisk ("*") character. 
-It will then pass the line to thread Munch2 through another queue of character strings.
+	Munch1 will scan the line and replace each space character (not tabs or newlines) with an asterisk ("*") character. 
+	It will then pass the line to thread Munch2 through another queue of character strings.
 */
