beb9d62f0eb2a35b2cdc037e2bbb41dc59e4ca3b
YSL-1997 <61572100+YSL-1997@users.noreply.github.com>
2020-09-28 16:24:30 -0500

    Update writer.c

--- p2/writer.c
+++ p2/writer.c
@@ -1,3 +1,9 @@
+/*
+	Authors:
+	Yusen Liu - NetID: liu797, CS login: yusen
+	Yuting Yan - NetID: yan86 , CS login: yutingy
+	Project #2 - Shared Memory Producer/Consumer Program
+*/
 
 /*
   Writer will write the line to standard output.
