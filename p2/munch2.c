98938de13f7c23a79f3f76149f35c27279fb1d2a
YSL-1997 <61572100+YSL-1997@users.noreply.github.com>
2020-09-28 16:16:33 -0500

    Update munch2.c

--- p2/munch2.c
+++ p2/munch2.c
@@ -4,6 +4,6 @@
 */
 
 /*
-Munch2 will scan the line and convert all lower case letters to upper case (e.g., convert "a" to "A"). 
-It will then pass the line to thread Writer though yet another queue of character strings.
+	Munch2 will scan the line and convert all lower case letters to upper case (e.g., convert "a" to "A"). 
+	It will then pass the line to thread Writer though yet another queue of character strings.
 */
