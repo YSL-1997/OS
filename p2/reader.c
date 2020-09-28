d82977eb75d1e73129e750ce35c5005fdd7a5735
YSL-1997 <61572100+YSL-1997@users.noreply.github.com>
2020-09-28 16:18:33 -0500

    Update reader.c

--- p2/reader.c
+++ p2/reader.c
@@ -6,4 +6,8 @@
 /*
 	The Reader thread will read from standard input, one line at a time. 
 	Reader will take the each line of the input and pass it to thread Munch1 through a queue of character strings.
+	
+	Each time that the Reader thread is going to read a string from standard input, it will first malloc memory for that string. 
+	The pointer to that string will then be passed down the queues between the threads until it finally arrives at the Writer thread. 
+	After Writer writes out the string pointed to by the pointer, it will then free the memory that holds the string.
 */
