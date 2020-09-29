041c6c8a73607eb77cb9f18c28fffa490f972e86
YSL-1997 <liuys1997@gmail.com>
2020-09-28 21:45:16 -0500

    Update reader.c
    not the correct implementation, but one way of using getline()

--- p2/reader.c
+++ p2/reader.c
@@ -13,3 +13,35 @@
 	The pointer to that string will then be passed down the queues between the threads until it finally arrives at the Writer thread. 
 	After Writer writes out the string pointed to by the pointer, it will then free the memory that holds the string.
 */
+
+/*
+	ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+	
+	getline()  reads  an  entire  line  from  stream,  storing  the address of the buffer containing the text into *lineptr.
+		The buffer is null-terminated and includes the newline character, if one was found.
+	If *lineptr is set to NULL and *n is set 0 before the call, then getline() will allocate a buffer for  storing the line.
+		This buffer should be freed by the user program even if getline() failed.
+	Alternatively,  before  calling  getline(),  *lineptr can contain a pointer to a malloc(3)-allocated buffer *n bytes in size.
+		If the buffer is not large enough to hold the line, getline() resizes it with realloc(3),  up‐dating *lineptr and *n as necessary.
+*/
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// not the correct implementation, but one way of using getline()
+int main(int argc, char* argv[]){
+	char* line = NULL; // used to store
+	size_t len = 0;
+	ssize_t num_read; // number of characters read from std_in
+
+	printf("Please enter a line:\n");
+
+	while((num_read = getline(&line, &len, stdin)) != -1){  // getline() allocates memory to the buffer - line
+		printf("%s\n", line);
+	}
+	free(line); // remember to free after the bufer - line has been used
+	return 0; 
+}
