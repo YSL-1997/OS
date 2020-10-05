a0d124c0b98a60e50b190fc9a0b561a2108d1a8f
yyt86 <56054533+yyt86@users.noreply.github.com>
2020-10-05 11:09:56 -0500

    Add files via upload

--- p2/reader.c
+++ p2/reader.c
@@ -2,14 +2,25 @@
 #include <stdlib.h>
 
 
-int(){
-    int MAX_BUFFER = 50960
-    char *buffer = malloc(MAX_BUFFER * sizeof(char))
+int Reader(){
+    int MAX_BUFFER = 50960;
+    char *buffer = malloc(MAX_BUFFER * sizeof(char));
+    char *p;
+    int c;
 
     while(fgets(buffer, MAX_BUFFER, stdin) != NULL){
-        if(){
-            fprintf(stderr, "Exceed the max length")
+        if((p = strchr(buf, "\n")) == NULL){
+            fprintf(stderr, "Input line too long.\n")
+            while((c = fgetc(fp) != EOF && c != '\n')){}
         }
+        *p = "\0"
 
+    }
+    if(feof(stdin)){
+        return 0
+    }
+    else{
+        perror("Could not get info from stdin")
+        exit(EXIT_FAILURE)
     }
 }
