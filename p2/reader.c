fdada45ba61b58139699dce2b5c57214f9d04d00
yyt86 <56054533+yyt86@users.noreply.github.com>
2020-10-06 19:02:50 -0500

    Add files via upload

--- p2/reader.c
+++ p2/reader.c
@@ -1,65 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <string.h>
 
+int main(){
+    int i = 0;
+    int j = 0;
+    FILE *fp;
+    int MAX_LENGTH = 1096;
+    char *buffer = malloc(MAX_LENGTH * sizeof(char));
+    char ch;
 
-int Reader(){
-    // int MAX_BUFFER = 50960;
-    // char *buffer = malloc(MAX_BUFFER * sizeof(char));
-    // char *p;
-    // int c;
-
-    // while(fgets(buffer, MAX_BUFFER, stdin) != NULL){
-    //     if((p = strchr(buf, "\n")) == NULL){
-    //         fprintf(stderr, "Input line too long.\n")
-    //         while((c = fgetc(stdin)) != EOF && c != '\n'){}
-    //     }
-    //     *p = "\0"
-
-    // }
-    // if(feof(stdin)){
-    //     return 0
-    // }
-    // else{
-    //     perror("Could not get info from stdin")
-    //     exit(EXIT_FAILURE)
-    // }
-
-
-    char *buf, *lbuf, *subbuf;
-	size_t len;
-    int MAX_LENGTH = 50960;
-
-	lbuf = NULL;
-	while ((buf = fgetln(stdin, &len))) {
-        if (len <= MAX_LENGTH){
-            if (buf[len - 1] == '\n')
-                buf[len - 1] = '\0';
-            else {
-                /* EOF without EOL, copy and add the NUL */
-                if ((lbuf = malloc(len + 1)) == NULL)
-                    err(1, NULL);
-                memcpy(lbuf, buf, len);
-                lbuf[len] = '\0';
-                buf = lbuf;
-		    }   
-        }
-
-        else{
-            if ((subbuf = malloc(MAX_LENGTH)) == NULL)
-				    err(1, NULL);
-			    memcpy(subbuf, buf, MAX_LENGTH-1);
-                subbuf[MAX_LENGTH-1] = '\0'
-                buf = subbuf;
-                free(subbuf)
-        }
-	
-	}
-	free(lbuf);
-    if(feof(stdin)){
-        return 0
+    if((fp = fopen("bigfile.txt", "r") ) == NULL){
+        perror("Could not get info from stdin");
+        exit(EXIT_FAILURE);
     }
-    
-	if (ferror(fp))
-		err(1, "fgetln");
+
+    while( ch != EOF){
+        
+        ch = getc(fp);
+
+        if(i >= MAX_LENGTH){
+            buffer[i -1] = '\0';
+            fprintf(stderr, "Input line too long.\n");
+            j++;
+            printf("line id: %d, %s \n", j, buffer);
+            while(ch != '\n' && ch != EOF){
+                ch = getc(fp) ;   
+            }
+            i = 0;
+            free(buffer);
+            buffer = malloc(MAX_LENGTH * sizeof(char));
+            continue;
+        }
+        buffer[i] = (char)ch;
+        i++;
+        if( ch == '\n' || ch == EOF){
+            buffer[i] = '\0';
+            // printf("%s", buffer);
+            // sem
+            // readerQ->EnqueueString(readerQ, buffer);
+            i = 0;
+            free(buffer);
+            buffer = malloc(MAX_LENGTH * sizeof(char));
+            j++;
+            if(ch == EOF)
+                break;
+        }
+
+
+    }
+
+    return 0;
 
 }
+
+
+int munch1(){
+    char *buffer = malloc(MAX_LENGTH * sizeof(char));
+    // readerQ->DequeueString(readerQ, buffer);
+    int i = 0;
+    if(buffer != NULL){
+        while(buffer[i] != '\0'){
+            if(buffer[i] == ' ')
+                buffer[i] = '*';
+            i++; 
+        }
+        // readerQ->EnqueueString(munchQ, buffer);}
+    }
+    // how to deal with the buffer = NULL
+}
+
+int munch2(){
+
+    char *buffer = malloc(MAX_LENGTH * sizeof(char));
+    // readerQ->DequeueString(readerQ, buffer);
+    int i = 0;
+    if(buffer != NULL){
+        while(buffer[i] != '\0'){
+            if(buffer[i]  >=97 && buffer[i] <=122)
+                buffer[i] -= 32;
+            i++; 
+        }
+        // readerQ->EnqueueString(munchQ, buffer);}
+    }
+    // how to deal with the buffer = NULL
+}
+
+int write(){
+    
+}
+
+    
+    
