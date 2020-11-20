82a53b0a9f71bd8090138a9f61993945c64338c9
yusen <liuys1997@gmail.com>
2020-11-20 13:14:44 -0600

    modified processes.h

--- p4/processes.h
+++ p4/processes.h
@@ -6,7 +6,19 @@ typedef struct process{
   // stores the pid of the process
 
   unsigned long start_index;
+  // stores the starting index of the pid
+  
   unsigned long end_index;
+  // stores the end index of the pid
+
+  unsigned long cur_index
+  // stores the current index of the pid that about to execute
+  // e.g. on line_index 0, pid 1 has a page fault, then pid_1->cur_index = 0
+  // then add to the I/O queue
+
+  process* next;
+  // this is used in the I/O queue
+  
 }process;
 
 
