cd933b4f27d3bcfd08350d6bbfd9c1c47fb3bc94
yyt86 <56054533+yyt86@users.noreply.github.com>
2020-12-10 21:06:07 -0600

    Add files via upload

--- p5/min.c
+++ p5/min.c
@@ -130,16 +130,26 @@ page *page_replace(page **ram_head,
                    void *pt, char *filename, void **proc_table, 
                    unsigned long frame_size)
 {
+  printf("page need to be replaced");
+  
   FILE *fp_min;                               // read trace file
   int MAX_LEN = 4096;                         // line max length
   char *buf = malloc(MAX_LEN * sizeof(char)); //store the line string
 
   char *cur_pid = NULL;
   char *cur_vpn = NULL;
-
+  // to store the page table search result
   node_pt *result_pt = NULL;
+
+  // create a hash table to store the <pid, vpn> pair as key and the offset of 
+  // fist trace with pid, vpn existing in the ram page as value
   ENTRY e, *ep;
   hcreate(frame_size);
+
+  // create a string array to store the hashtable key, it will be used to free 
+  // the key of hashtable
+  char* key_array[frame_size];
+  unsigned long key_index = 0;
 
   fp_min = read_file(filename);
 
@@ -164,6 +174,8 @@ page *page_replace(page **ram_head,
     }
     else
     {
+      //check whether current location is less than the current index of
+      // page, if so, the current trace has been implemented, then skip.
       if (result_proc->value->cur_index >
           (unsigned)handle_ftell_error(ftell(fp_min)))
       {
@@ -174,10 +186,13 @@ page *page_replace(page **ram_head,
       }
       else
       {
+        
         char *key_pt = get_key_pt(cur_pid, cur_vpn);
         // free(cur_pid);
         result_pt = find_pt(&pt, key_pt);
         // free(key_pt);
+
+        // check whether the trace is in ram
         if (result_pt == NULL)
         {
           free(cur_pid);
@@ -188,19 +203,32 @@ page *page_replace(page **ram_head,
         else
         {
           e.key = key_pt;
+          // if the trace was in ram but not in the hashtable, that means the 
+          // trace is one of the first pid, vpn existing in ram but not be hitted 
           if (hsearch(e, FIND) == NULL)
           {
             e.data = (void *)handle_ftell_error(ftell(fp_min));
             hsearch(e, ENTER);
+
+            // store the key pointer of the hash table
+            key_array[key_index] =  e.key;
+            key_index++;
+
+            free(cur_pid);
+            free(cur_vpn);
+            free(pid_vpn_pair);
           }
+          // if the pid, vpn were in hashtable, that means it is not the first 
+          // trace not be  read but exists in ram
           else
           {
             free(key_pt);
+            free(cur_pid);
+            free(cur_vpn);
+            free(pid_vpn_pair);
             continue;
           }
-          free(cur_pid);
-          free(cur_vpn);
-          free(pid_vpn_pair);
+          
         }
       }
     }
@@ -209,28 +237,39 @@ page *page_replace(page **ram_head,
   page *temp = *ram_head;
   long max_offset = 0;
   page *ret = NULL;
+  // loop the ram list/ the hashtable
   while (temp != NULL)
   {
     e.key = get_key_pt(temp->pid, temp->vpn);
     ep = hsearch(e, FIND);
+    // if the ram page were not in hashtable, that means there is no trace with 
+    //same pid, vpn as the page, so the page can be replaced
     if (ep == NULL)
     {
       ret = temp;
       free(e.key);
       break;
     }
+    // if the ram page were in hashtable, compare the offset of the same trace
+    // and if it was larger than the current max, reset return page and max
     else
     {
       if ((long)ep->data > max_offset)
       {
         ret = temp;
-        max_offset = (unsigned long)ep->data;
-        free(e.key);
+        max_offset = (unsigned long)ep->data; 
       }
+      free(e.key);
     }
   }
 
   hdestroy();
 
+  for(unsigned long i = 0; i < key_index; i++){
+    free(key_array[i]);
+  }
+  // free(key_array);
+  free(buf);
+  printf("replace pid and vpn: %s %s \n", ret->pid, ret->vpn);
   return ret;
 }
